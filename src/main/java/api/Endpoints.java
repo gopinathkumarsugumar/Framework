fb0b56b64c259dba03d7543665590db8432627da
gopinathkumarsugumar <gopinathkumar.sugumar@gmail.com>
2026-09-19 22:51:58 +0100

    Delete src directory

--- src/main/java/api/Endpoints.java
+++
@@ -1,8 +0,0 @@
-package api;
-
-import utils.ConfigReader;
-
-public class Endpoints {
-    public static String collections = "/collections";
-    public static String createNewCollectionName = "/collections/{collectionName}/objects";
-}
